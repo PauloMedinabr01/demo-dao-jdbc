625dc00b4a3bb987306db3d5b204d4d7a361392a
Paulomedinabr01 <paulomedinabr01@gmail.com>
2023-02-13 15:33:33 -0300

    findByDepartment implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -23,11 +23,5 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
-
-		System.out.println("\n=== TEST 3: seller findAll =====");
-		list = sellerDao.findAll();
-		for (Seller obj : list) {
-			System.out.println(obj);
-		}
 	}
 }
